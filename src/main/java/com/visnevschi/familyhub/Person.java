01fd88bef8b8c3e59d39da8484abc942d8427568
VisnevschiV <56790828+VisnevschiV@users.noreply.github.com>
2025-12-08 15:56:45 +0100

    fixed infinite recursion

--- src/main/java/com/visnevschi/familyhub/Person.java
+++ src/main/java/com/visnevschi/familyhub/Person.java
@@ -1,5 +1,6 @@
 package com.visnevschi.familyhub;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
@@ -22,6 +23,7 @@ public class Person {
 
     @JoinColumn(name = "family_id")
     @ManyToOne
+    @JsonBackReference
     private Family family;
 
     // Standard Constructors
