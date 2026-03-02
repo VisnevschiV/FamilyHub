4dbfb04c99601b6ca90774d0f6f292a103abef7c
VisnevschiV <56790828+VisnevschiV@users.noreply.github.com>
2026-03-02 20:36:15 +0100

    update Cors to accept calls from new front

--- src/main/java/com/visnevschi/familyhub/config/CorsConfig.java
+++ src/main/java/com/visnevschi/familyhub/config/CorsConfig.java
@@ -1,6 +1,7 @@
 package com.visnevschi.familyhub.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,13 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
                         "http://localhost:3000",  // React default
                         "http://localhost:5173",  // Vite default
-                        "http://localhost:4200"   // Angular default
+                "http://localhost:4200",  // Angular default
+                "https://victorious-rock-0dc461310.6.azurestaticapps.net",
+                "https://familyhub-gte6cabtbg.net"
                 )
+            .allowedOriginPatterns("https://*.azurestaticapps.net")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
