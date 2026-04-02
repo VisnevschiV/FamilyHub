a4eb6ff583dccf1f202fbee3c20567022b844744
VisnevschiV <56790828+VisnevschiV@users.noreply.github.com>
2026-04-02 10:49:03 +0200

    H2 on tests

--- src/test/java/com/visnevschi/familyhub/FamilyHubApplicationTests.java
+++ src/test/java/com/visnevschi/familyhub/FamilyHubApplicationTests.java
@@ -2,8 +2,17 @@ package com.visnevschi.familyhub;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.datasource.url=jdbc:h2:mem:familyhub_test;MODE=PostgreSQL;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+    "spring.jpa.hibernate.ddl-auto=create-drop"
+})
+@ActiveProfiles("test")
 class FamilyHubApplicationTests {
 
     @Test
